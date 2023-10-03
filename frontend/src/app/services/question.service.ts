0dd956fab7ed744defa0fec906735b0c57ffe048
Marcos Visentini <marcosvisentini@gmail.com>
2023-10-03 18:07:39 -0300

    Introduz variáveis de ambiente para evitar conflitos futuramente

--- frontend/src/app/services/question.service.ts
+++ frontend/src/app/services/question.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
 import { Statement } from '../models/statement';
 import { Personality } from '../models/personality';
-import { map } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
 
 interface ApiResponse {
   success: boolean;
+
   httpStatus: {
     name: string;
     code: number;
   };
+
   data: Statement;
+
   error: any;
+  
   timestamp: string;
 }
 
@@ -27,12 +33,12 @@ export class QuestionService {
   constructor(private http: HttpClient) { }
 
   getStatement(id: number): Observable<Statement> {
-    const url = `http://localhost:3000/api/statements/${id}`;
+    const url = `${environment.apiBaseURL}/statements/${id}`;
     return this.http.get<ApiResponse>(url).pipe(map(response => response.data));
   }
 
   getPersonality(id: number): Observable<Personality> {
-    const url = `http://localhost:3000/api/personalities/${id}`;
+    const url = `${environment.apiBaseURL}/personalities/${id}`;
     return this.http.get<Personality>(url);
   }
 

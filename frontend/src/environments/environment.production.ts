236c7465061394cb80dab269dbd041e6c003197e
Marcos Visentini <marcosvisentini@gmail.com>
2023-10-04 01:05:12 -0300

    Corrige apiBaseURL do ambiente de produção

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseURL: `${window.location.protocol}//${window.location.hostname}${window.location.port}/api`
+  apiBaseURL: `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api`
 };

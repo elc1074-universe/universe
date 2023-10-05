48f51eb31b653aab531d07b6ab8d63443b7d35d4
larissa <larissapretto009@gmail.com>
2023-10-05 15:51:08 -0300

    endpoint game em andamento

--- backend/src/app/controllers/OptionController.ts
+++ backend/src/app/controllers/OptionController.ts
@@ -1,14 +1,24 @@
-import { Router, Request, Response, RequestHandler, NextFunction } from 'express';
-import { StatusCodes } from 'http-status-codes';
+import {
+  Router,
+  Request,
+  Response,
+  RequestHandler,
+  NextFunction,
+} from "express";
+import { StatusCodes } from "http-status-codes";
 
-import OptionService from '../services/OptionService';
-import Option from '../entities/database/Option';
-import OptionRetrievalDTO from '../entities/dto/OptionRetrievalDTO';
-import ApiResponse from '../entities/api/ApiResponse';
+import OptionService from "../services/OptionService";
+import Option from "../entities/database/Option";
+import OptionRetrievalDTO from "../entities/dto/OptionRetrievalDTO";
+import ApiResponse from "../entities/api/ApiResponse";
 
 const optionRouter: Router = Router();
 
-optionRouter.get('/:id', (async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+optionRouter.get("/:id", (async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id = Number(request.params.id);
 
@@ -18,7 +28,8 @@ optionRouter.get('/:id', (async (request: Request, response: Response, next: Nex
 
     const statusCode: number = StatusCodes.OK;
 
-    const apiResponse: ApiResponse<OptionRetrievalDTO> = new ApiResponse<OptionRetrievalDTO>(statusCode, mappedOption);
+    const apiResponse: ApiResponse<OptionRetrievalDTO> =
+      new ApiResponse<OptionRetrievalDTO>(statusCode, mappedOption);
 
     response.status(statusCode).json(apiResponse);
   } catch (error: unknown) {
@@ -26,17 +37,27 @@ optionRouter.get('/:id', (async (request: Request, response: Response, next: Nex
   }
 }) as RequestHandler);
 
-optionRouter.get('/statement/:id', (async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+optionRouter.get("/statement/:id", (async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id = Number(request.params.id);
 
-    const rawOptions: Option[] = await OptionService.findOptionsByStatementId(id);
+    const rawOptions: Option[] = await OptionService.findOptionsByStatementId(
+      id
+    );
 
-    const mappedOptions: OptionRetrievalDTO[] = rawOptions.map(option => new OptionRetrievalDTO(option));
+    const mappedOptions: OptionRetrievalDTO[] = rawOptions.map(
+      (option) => new OptionRetrievalDTO(option)
+    );
 
     const statusCode: number = StatusCodes.OK;
 
-    const apiResponse: ApiResponse<OptionRetrievalDTO[]> = new ApiResponse<OptionRetrievalDTO[]>(statusCode, mappedOptions);
+    const apiResponse: ApiResponse<OptionRetrievalDTO[]> = new ApiResponse<
+      OptionRetrievalDTO[]
+    >(statusCode, mappedOptions);
 
     response.status(statusCode).json(apiResponse);
   } catch (error: unknown) {

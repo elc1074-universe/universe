88195eb91fae1d5d832eebf5fd597f78b0eed3d3
Marcos Visentini <marcosvisentini@gmail.com>
2023-10-27 19:52:33 -0300

    Cria script para automatizar o deploy dentro do OpenStack

--- deploy-frontend.sh
+++
@@ -1,9 +0,0 @@
-cd ./frontend
-
-npm install
-
-ng build
-
-sudo rm -rf /usr/share/nginx/html/*
-
-sudo cp -r ./dist/frontend/* /usr/share/nginx/html
---
+++ deploy.sh
@@ -0,0 +1,13 @@
+git pull
+
+cd backend
+
+npm run pm2
+
+cd ../
+
+cd frontend
+
+npm run nginx
+
+cd ../

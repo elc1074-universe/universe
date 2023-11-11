2b6f2c449bbf7f41493c48bac97f443afd192a1f
Marcos Visentini <marcosvisentini@gmail.com>
2023-11-11 11:08:08 -0300

    Volta a usar CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,19 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
+import { StatusCodes } from 'http-status-codes';
 
 import config from './config/config';
 import dataSource from './database/data-source';
 import routes from './app/routes/routes';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost',
+  optionsSuccessStatus: StatusCodes.OK
+}));
 
 app.use(express.json());
 

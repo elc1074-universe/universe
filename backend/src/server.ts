61fd1b22b41bb8e31ca674da0dfaa5ee7f879069
Marcos Visentini <marcosvisentini@gmail.com>
2023-11-11 11:10:22 -0300

    Aprimora configuração do CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ import routes from './app/routes/routes';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost',
+  origin: 'http://universe.inf.ufsm.br',
   optionsSuccessStatus: StatusCodes.OK
 }));
 

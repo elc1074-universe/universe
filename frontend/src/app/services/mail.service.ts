3901b4df3b67d53dd214f4af4e0b9bb0dee22195
Enzo Becker <ezbecker@inf.ufsm.br>
2023-11-16 23:34:27 -0300

    corrige url da api para envio de email

--- frontend/src/app/services/mail.service.ts
+++ frontend/src/app/services/mail.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class MailService {
   constructor(private http: HttpClient) {}
 
   sendMail(to: string, subject: string, text: string): Observable<any> {
-    return this.http.post("http://localhost:3000/api/mail/send", {
+    return this.http.post(`${environment.apiBaseURL}/mail/send`, {
       to,
       subject,
       text,
